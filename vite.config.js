682a9797a5cc6e78ee5580c0a0e2aba471ca6c68
Ale Bavaro - Ale Gondra <bavaroalejandro@gmail.com>
2025-08-14 23:24:43 -0300

    arreglando error

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets')
+    }
+  },
   build: {
     outDir: 'dist',
+    emptyOutDir: true, // Limpia el directorio de build previo
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'js/[name].[hash].js',
+        entryFileNames: 'js/[name].[hash].js'
+      }
+    }
   },
   server: {
     port: 3000,
     open: true,
+    strictPort: true
   },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  optimizeDeps: {
-    include: [
-      'react-big-calendar',
-      'moment',
-      'react-big-calendar/lib/css/react-big-calendar.css'
-    ],
-    exclude: []
+  preview: {
+    port: 4173,
+    open: true
   }
 });
